785fc6ae6ad007c8fe1905bdffdb5a7cbcdb65e8
koushik4455 <gaddamkoushik480@gmail.com>
2024-02-16 14:16:47 +0530

    project

---
+++ src/Components/MealItem.jsx
@@ -0,0 +1,42 @@
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+// const MealItem=({data})=>{
+//     console.log(data);
+//     let navigate = useNavigate();
+//     return(
+//         <>  
+//             {
+//                 (!data) ?"Not Found": data.map(item=>{
+//                     return(
+//                     <div className="card" key={item.idMeal} onClick={()=>navigate(`/${item.idMeal}`)}>
+//                         <img src={item.strMealThumb} alt="" />
+//                         <h3>{item.strMeal}</h3>
+//                     </div>
+//                     )
+//                 })
+//             }
+            
+//         </>
+//     )
+// }
+// export default MealItem;
+// MealItem.js
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const MealItem = ({ data }) => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            {data.map(item => (
+                <div className="card" key={item.idMeal} onClick={() => navigate(`/${item.idMeal}`)}>
+                    <img src={item.strMealThumb} alt="" />
+                    <h3>{item.strMeal}</h3>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default MealItem;
